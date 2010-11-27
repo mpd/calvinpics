66188ded795ec140f792f38e88b2afd0f3893821
Michael Davidson <mdavids@gmail.com>
2010-11-27 13:01:50 -0800

    Initial commit, four files + manifest.
    Does nothing, but is all hooked up.

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+ hideOverlay();
+};
+
+function showOverlay() {
+  document.getElementById('overlay').style.display = '';
+}
+
+function hideOverlay() {
+  document.getElementById('overlay').style.display = 'none';
+};
